69705a38891bb17a1e42ab1e5d6fc850be3b9ba5
ReDay Zarra <redayzarra@gmail.com>
2023-01-07 01:04:52 -0500

    Adding git ignore to remove bugs

--- Traffic_Signs_Classification.py
+++ Traffic_Signs_Classification.py
@@ -19,3 +19,35 @@ import seaborn as sns
 
 # Using pickle package to open our data, not much use after that
 import pickle
+
+
+"""
+Importing the data into three different sections: training, validation, and
+testing. Training data is used to train the network, testing data is used to test
+the trained network with data that it has never seen to see how it would perform in
+the real world. The validation dataset ensures that we avoid overfitting by showing
+the network validation data every epoch (or run) in a process called 
+cross-validation to ensure that the network is not focusing on the details of the
+training data. 
+"""
+with open("./traffic-signs-data/train.p", mode = 'rb') as training_data:
+  train = pickle.load(training_data) # Use pickle's load method to load the defined data as the variable test
+
+with open("./traffic-signs-data/valid.p", mode = 'rb') as validation_data:
+  valid = pickle.load(validation_data)
+
+with open("./traffic-signs-data/test.p", mode = 'rb') as testing_data:
+  test = pickle.load(testing_data)
+
+
+"""
+Splitting the data into our individual training and testing set variables.
+"""
+# Assigning the features of the training set as X_train and the dependent variable (labels) as y_train
+X_train, y_train = train['features'], train['labels']
+
+# Assigning the features of the validation set as X_validation and the dependent variable (labels) as y_validation
+X_validation, y_validation = valid['features'], valid['labels']
+
+# Assigning the features of the testing set as X_test and the dependent variable (labels) as y_test
+X_test, y_test = test['features'], test['labels']
